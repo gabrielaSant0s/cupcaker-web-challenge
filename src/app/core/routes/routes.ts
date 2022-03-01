13b48e4d47a7476891007e6c9d7146a940f11f78
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-03-01 02:00:54 -0300

    change

--- src/app/core/routes/routes.ts
+++ src/app/core/routes/routes.ts
@@ -1,7 +1,9 @@
 import { AppRoute } from './types'
 import Home from 'app/core/pages/home'
 
+
 export const coreRoutes: AppRoute[] = [
   { path: '/', exact: true, component: Home },
   { path: '/private', exact: true, component: Home, isPrivate: true },
+  
 ]
