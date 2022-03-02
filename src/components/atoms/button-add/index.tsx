63caa651c3f61def9fa4103a495a2837393184b3
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-03-02 01:17:36 -0300

    TASK-005_create_component_buyyon_add_coin: mutavel

--- src/components/atoms/button-add/index.tsx
+++ src/components/atoms/button-add/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Container, Button, Typography } from "@mui/material";
 
-const label = "ADD NEW COIN"
 
-const BotaoAdd = (): JSX.Element => {
+
+const BotaoAdd = ({label = "ADD NEW COIN"}): JSX.Element => {
     return(
        <Container maxWidth="sm">
            <Button variant="contained" 
@@ -16,3 +16,4 @@ const BotaoAdd = (): JSX.Element => {
 }
 
 export default BotaoAdd
+
