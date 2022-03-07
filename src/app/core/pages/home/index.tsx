92880f91b037d41f263a57fbed8ab8c44d14d88e
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-03-07 01:21:43 -0300

    TASK-007_componente_row_list

--- src/app/core/pages/home/index.tsx
+++ src/app/core/pages/home/index.tsx
@@ -10,11 +10,23 @@ import ButtonAdd from "../../../../components/atoms/button-add"
 
 import { Grid } from '@mui/material'
 import { useHistory } from 'react-router-dom'
+import RowCoin from 'components/molecules/row-coin'
+import ConnectSelect from 'contexts'
 
 const Home = (): JSX.Element => {
+  const {sendForm} = useContext(ConnectSelect)
   const history = useHistory()
 
+  const price = sendForm["price"]
   
+  function Preencher(price=0){
+    
+    if(price == 0){
+      return <EmptyState type={EmptyStateType.emptyWallet} />
+    }else{
+      return <RowCoin/>
+    }
+  }
   
   return (
     
@@ -36,8 +48,14 @@ const Home = (): JSX.Element => {
           </Grid>
         </Grid>
         
+        
         <Panel className={styles.panel}>
-          <EmptyState type={EmptyStateType.emptyWallet} />
+          
+          <Grid>
+            {Preencher(price)}
+          </Grid>
+          
+         
         </Panel>
         <PegaForm/>
       </div>
---
+++ src/components/molecules/row-coin/index.tsx
@@ -0,0 +1,47 @@
+import { Grid, Typography} from "@mui/material";
+import { Box } from "@mui/system";
+import LetraInicial from "components/atoms/letra-inicial";
+import ConnectSelect from "contexts";
+import React, { useContext } from "react";
+
+const RowCoin = (): JSX.Element => {
+    const {sendForm} = useContext(ConnectSelect)
+    const price = sendForm["price"]
+    const date = sendForm["date"]
+    const name = sendForm["name"]
+    const quant = sendForm["quantity"]
+    
+    const multi = quant*price
+    
+
+    return(
+    <Grid container sx={{width:"70em", marginBottom:"40%",direction:"row", height:"7.5em", borderBottom:"solid 2px #EEEEEE"}}>
+        <Grid sx={{ marginLeft:"5%"}}>
+            <LetraInicial/>
+        </Grid>
+        <Grid container 
+                direction="column"
+                justifyContent="space-between"
+                alignItems="flex-start" 
+                sx={{ marginLeft:"5%", width:"25%"}}>
+            <Box sx={{marginRight:"10%"}}>
+                <Typography  sx={{fontFamily:"inter", fontWeight:"500", fontSize:"2em"}}>{name}</Typography>
+            </Box>
+            <Box>
+                <Typography  sx={{fontFamily:"inter", fontWeight:"400",fontSize:"1em", marginBottom:"20%"}}>{date}</Typography>
+            </Box>
+            
+        </Grid>
+        <Grid container justifyContent="center" sx={{bgcolor:"#0058FF", width:"20%", height:"35%", marginLeft:"30%", marginTop:"3%", borderRadius:"16px"}}>
+        <Typography  sx={{fontFamily:"inter", fontWeight:"400",fontSize:"1.2em", marginBottom:"20%", color:"#FFFFFF", marginTop:"3%"}}>
+            R${multi}</Typography>
+            
+        </Grid>
+           
+        
+    </Grid>
+        
+    )
+}
+
+export default RowCoin
